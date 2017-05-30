ef73e2106953df824ba215015abc32e221c804b9
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2017-05-30 12:04:20 +0900

    Add files via upload

--- assets/custom/js/contactus.js
+++ assets/custom/js/contactus.js
@@ -7,59 +7,24 @@ $(document).ready(function() {
         var user_email      = $('input[name=email]').val();
         var user_phone      = $('input[name=phone]').val();
         var user_message    = $('textarea[name=message]').val();
-       
-        //simple validation at client's end
+       	
+		//simple validation at client's end
         //we simply change border color to red if empty field using .css()
-        var proceed = true;
         if(user_name==""){
             $('input[name=name]').css('border-color','red');
-            proceed = false;
-        }
-        if(user_email==""){
+            return false;
+        }else if(user_email==""){
             $('input[name=email]').css('border-color','red');
-            proceed = false;
-        }
-        if(user_phone=="") {    
+            return false;
+        }else if(user_phone=="") {    
             $('input[name=phone]').css('border-color','red');
-            proceed = false;
-        }
-        if(user_message=="") {  
+            return false;
+        }else if(user_message=="") {  
             $('textarea[name=message]').css('border-color','red');
-            proceed = false;
-        }
-
-        //everything looks good! proceed...
-        if(proceed)
-        {
-            //data to be sent to server
-            post_data = {'userName':user_name, 'userEmail':user_email, 'userPhone':user_phone, 'userMessage':user_message};
-           
-            //Ajax post data to server
-            $.post('contact_me.php', post_data, function(response){  
-               
-                //load json data from server and output message    
-                if(response.type == 'error')
-                {
-                    output = '<div class="alert alert-danger  fa fa-exclamation-triangle">'+response.text+'</div>';
-                }else{
-               
-                    output = '<div class="alert alert-success">'+response.text+'</div>';
-                   
-                    //reset values in all input fields
-                    $('#contact_form input').val('');
-                    $('#contact_form textarea').val('');
-                }
-               
-                $("#result").hide().html(output).slideDown();
-            }, 'json');
-           
-        }
+            return false;
+        }else{
+			$("#result").html('<div class="alert alert-success"> Message has been sent successfully.</div>');
+		}
+		
     });
-   
-    //reset previously set border colors and hide all message on .keyup()
-    $("#contact_form input, #contact_form textarea").keyup(function() {
-        $("#contact_form input, #contact_form textarea").css('border-color','');
-        $("#result").slideUp();
-    });
-   
 });

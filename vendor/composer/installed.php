cd50f8f01c4b2851175711b1569e5ac668f90c53
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2021-04-23 10:04:37 +0900

    create composer
    add composer.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,26 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'mianzaid/html-template-free',
+    'dev' => true,
+  ),
+  'versions' => 
+  array (
+    'mianzaid/html-template-free' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+      'dev-requirement' => false,
+    ),
+  ),
+);
